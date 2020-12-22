96b27b6ae3c37d2c49d2304d05214f1dcd9921c5
Evgeny Shalnov <shalnov.eugen@gmail.com>
2020-12-22 15:29:24 +0300

    fix bug in starting container

--- docker_build.sh
+++ docker_build.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
-docker build . -t cambot:latest
+set -x -e
+
+script_dir=$(realpath $(dirname $0))
+docker build $script_dir -t cambot:latest
--- docker_run.sh
+++ docker_run.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 if [ $# -ne 2 ]; then
-    echo "Usage: $0 <camera_id> <token_path>"
+    echo "Usage: $0 <camera_id> <token>"
     exit -1
 fi
 
 set -x -e
 
 CAM_ID=$1
-TOKEN_PATH=$2
+TOKEN=$2
 
 script_path=$(realpath $(dirname $0))
 config_dir_path=$(mktemp -d)
@@ -16,5 +16,5 @@ config_file=${config_dir_path}/config.json
 log_path=${script_path}/logs
 
 
-echo "{ \"token\": \"$(cat $TOKEN_PATH)\", \"camera_id\": 0 }" >$config_file
+echo "{ \"token\": \"$TOKEN\", \"camera_id\": 0 }" >$config_file
 docker run -d -v ${config_dir_path}:/options -v ${log_path}:/logs --device=/dev/video${CAM_ID}:/dev/video0 cambot:latest
