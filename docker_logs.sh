d438d0b7791a317dae0b0edc781e2342fe31ec3a
Evgeny Shalnov <shalnov.eugen@gmail.com>
2021-03-16 18:13:27 +0300

    update logger

--- docker_logs.sh
+++ docker_logs.sh
@@ -1,2 +1,14 @@
 #!/bin/bash
-docker logs $(docker container ls | grep "cambot:latest" | cut -d' ' -f1)
+CONTAINER_NAME=$(docker container ls | grep "cambot:latest" | cut -d' ' -f1)
+if [[ -z "$CONTAINER_NAME" ]]; then
+    CONTAINER_NAME=$(docker container ls -a | grep "cambot:latest" | cut -d' ' -f1 | head -n1)
+    if [[ -z "$CONTAINER_NAME" ]]; then
+        echo "No container found"
+        exit -1
+    else
+        echo "Stopped container $CONTAINER_NAME"
+    fi
+else
+    echo "Running container $CONTAINER_NAME"
+fi
+docker logs $CONTAINER_NAME
